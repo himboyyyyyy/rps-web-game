a4fbf5a312b8b4ed6acf8d3efa8ed1babf17191f
qz_ <himhimboy1008@gmail.com>
2022-07-26 12:24:02 +0800

    a

--- src/game.js
+++ src/game.js
@@ -1,11 +1,53 @@
 function scissor() {
-    alert("you choose scissor!")
+    getResult("scissor")
 }
 
 function paper() {
-    alert("you choose paper!")
+    getResult("paper")
 }
 
 function rock() {
-    alert("you choose rock!")
+    getResult("rock")
+}
+
+function getResult(choice) {
+    array = ["scissor", "paper", "rock"]
+    com = array[Math.floor(Math.random()*array.length)];
+    if (choice == com) {
+        const result = document.getElementById("result");
+        result.innerHTML = "TIE!";
+    } 
+    if (choice == "scissor") {
+        if (com == "paper") {
+            const result = document.getElementById("result");
+            result.innerHTML = "YOU WIN! your choice:" + choice + "computer choice:" + com;
+        } else if (com == "rock") {
+            const result = document.getElementById("result");
+            result.innerHTML = "YOU LOSE! your choice:" + choice + "computer choice:" + com;
+        }
+    }
+
+    if (choice == "paper") {
+        if (com == "rock") {
+            const result = document.getElementById("result");
+            result.innerHTML = "YOU WIN! your choice:" + choice + "computer choice:" + com;
+        } else if (com == "scissor") {
+            const result = document.getElementById("result");
+            result.innerHTML = "YOU LOSE! your choice:" + choice + "computer choice:" + com;
+        }
+    }
+
+    if (choice == "rock") {
+        if (com == "scissor") {
+            const result = document.getElementById("result");
+            result.innerHTML = "YOU WIN! \nyour choice:" + choice + "computer choice:" + com;
+        } else if (com == "paper") {
+            const result = document.getElementById("result");
+            const choice = document.getElementById("user");
+            const com = document.getElementById("com");
+            result.innerHTML = "YOU LOSE!";
+            choice.innerHTML = "you choice:" + choice;
+            com.innerHTML = "YOU LOSE!";
+        }
+    }
 }
